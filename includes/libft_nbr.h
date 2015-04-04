f27504133196a818d82d4859385c1cbd7f45c060
zipeldiablo <tnoah@student.42.fr>
2015-04-04 15:16:10 +0200

    changes lib

--- includes/libft_nbr.h
+++ includes/libft_nbr.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   libft_nbr.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tnoah <marvin@42.fr>                       +#+  +:+       +#+        */
+/*   By: zipeldiablo <zipeldiablo@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/02/04 18:30:26 by tnoah             #+#    #+#             */
-/*   Updated: 2015/02/04 18:30:27 by tnoah            ###   ########.fr       */
+/*   Updated: 2015/04/04 14:59:31 by zipeldiablo      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,7 @@
 
 char				*ft_itoa(int n);
 int					ft_atoi(const char *str);
+long long			ft_atol(const char *str);
+size_t				ft_intlen(int nbr);
 
 #endif
