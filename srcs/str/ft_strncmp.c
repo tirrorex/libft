d596f29b9eb375f2e8144466326940301114dfe7
Thibaut Noah <tnoah@student.42.fr>
2015-11-19 10:54:07 +0100

    github

--- srcs/str/ft_strncmp.c
+++ srcs/str/ft_strncmp.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strncmp.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tnoah <marvin@42.fr>                       +#+  +:+       +#+        */
+/*   By: zipeldiablo <zipeldiablo@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/04 14:33:17 by tnoah             #+#    #+#             */
-/*   Updated: 2014/11/28 15:52:45 by tnoah            ###   ########.fr       */
+/*   Updated: 2015/06/12 22:10:49 by zipeldiablo      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,11 @@
 
 int		ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	size_t		i;
-
 	if (s1 && s2)
 	{
-		i = 0;
 		while (n > 0)
 		{
-			if (s1[i] != s2[i])
+			if (*s1 != *s2)
 				return ((*(unsigned char*)s1 < *(unsigned char *)s2) ? -1 : +1);
 			else if (*s1 == '\0')
 				return (0);
